91f7af48d42cbb7882abe7af69cea2662e43abe2
saniok333 <alex.o.csc@gmail.com>
2022-06-15 15:52:14 +0300

    Users fetching implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { UserList } from './components/UserList';
+
 function App() {
-  return <div>fuck</div>;
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { Dispatch } from 'react';
+import { UserAction, UserActionTypes } from '../../types/users';
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      dispatch({ type: UserActionTypes.FETCH_USERS });
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setTimeout(
+        () =>
+          dispatch({
+            type: UserActionTypes.FETCH_USERS_SUCCESS,
+            payload: response.data,
+          }),
+        500
+      );
+    } catch (error) {
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_ERROR,
+        payload: 'An error occurred while loading users',
+      });
+    }
+  };
+};
